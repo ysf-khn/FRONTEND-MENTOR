1f6a30e82c2d0459d404fdcf15ca1e9543ca18eb
Yusuf <yusufmohd72@gmail.com>
2021-09-15 22:14:50 +0530

    Added Calculation functionality

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,4 +1,62 @@
 'use strict';
 
-//     Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
-//     person Total / person Reset
+const billAmount = document.querySelector('.bill-value');
+const peopleCount = document.querySelector('.people-count');
+const tipPercent = document.querySelectorAll('.tip');
+const tipCustom = document.querySelector('.tip-custom-value');
+const tipPerPerson = document.querySelector('.tip-per-person');
+const totalPerPerson = document.querySelector('.total-per-person');
+const btnReset = document.querySelector('.reset');
+
+let bill, tip, totalPeople;
+
+tipPercent.forEach(button => {
+  button.addEventListener('click', () => {
+    if (!button.classList.contains('tip-custom')) {
+      tip = +button.innerHTML.slice(0, -1);
+      tip /= 100;
+    }
+    bill = +billAmount.value;
+    button.classList.add('btn-highlight');
+  });
+});
+
+peopleCount.addEventListener('keyup', () => {
+  totalPeople = +peopleCount.value;
+
+  if (typeof tip !== 'Number') {
+    tip = +tipCustom.value.slice(0, -1);
+    tip /= 100;
+  }
+  console.log(bill, tip, totalPeople);
+
+  if (totalPeople) {
+    const totalTip = tip * bill;
+    console.log(totalTip);
+
+    const tipAmountPerPerson = (totalTip / totalPeople).toFixed(2);
+    tipPerPerson.innerHTML = `$${tipAmountPerPerson}`;
+
+    const totalBillPerPerson = (
+      bill / totalPeople +
+      Number(tipAmountPerPerson)
+    ).toFixed(2);
+    totalPerPerson.innerHTML = `$${totalBillPerPerson}`;
+  } else {
+    tipPerPerson.innerHTML = `$0.00`;
+    totalPerPerson.innerHTML = `$0.00`;
+  }
+});
+
+btnReset.addEventListener('click', () => {
+  billAmount.value = '';
+  peopleCount.value = '';
+  tipPerPerson.innerHTML = `$0.00`;
+  totalPerPerson.innerHTML = `$0.00`;
+  tipCustom.value = '';
+
+  tipPercent.forEach(btn => {
+    if (btn.classList.contains('btn-highlight'))
+      btn.classList.remove('btn-highlight');
+  });
+});
